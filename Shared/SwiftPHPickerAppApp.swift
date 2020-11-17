cc801ef1ff65dc3327172c14ee1bef7a0bbc8eb4
Jan Hovland <jan.hovland@lyse.net>
2020-11-17 18:41:40 +0100

    Initial Commit

---
+++ Shared/SwiftPHPickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPHPickerAppApp.swift
+//  Shared
+//
+//  Created by Jan Hovland on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPHPickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
